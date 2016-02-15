b3eeddfab4de317094610810938cbca2e779e006
Brandon Bradley <bradleytastic@gmail.com>
2016-02-15 11:41:50 -0600

    [polling] collect data from OKCoin

--- src/test/scala/ExchangeServiceSpec.scala
+++ src/test/scala/ExchangeServiceSpec.scala
@@ -2,12 +2,17 @@ package co.coinsmith.kafka.cryptocoin
 
 import com.xeiam.xchange.bitfinex.v1.BitfinexExchange
 import com.xeiam.xchange.bitstamp.BitstampExchange
+import com.xeiam.xchange.okcoin.OkCoinExchange
 
 
 class ExchangeServiceSpec extends KafkaCryptocoinFunSpec {
   describe("ExchangeService") {
     it("should load all exchanges") {
-      val expected = Set(classOf[BitfinexExchange], classOf[BitstampExchange])
+      val expected = Set(
+        classOf[BitfinexExchange],
+        classOf[BitstampExchange],
+        classOf[OkCoinExchange]
+      )
       val result = ExchangeService.getExchanges.map(_.getClass).toSet
       assert(expected == result)
     }
