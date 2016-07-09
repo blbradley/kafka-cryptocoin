d54149b663f0a3cb3335c7ded47b64ec5d97f665
Brandon Bradley <bradleytastic@gmail.com>
2016-07-09 17:01:13 -0500

    [Streaming][Bitfinex] initial implementation (#47)
    * Tests were not written because of possible data format changes.
    * Introduces topic per exchange and data type.
    * Introduces dotted topic names.

--- src/main/scala/co/coinsmith/kafka/cryptocoin/StreamingActor.scala
+++ src/main/scala/co/coinsmith/kafka/cryptocoin/StreamingActor.scala
@@ -1,13 +1,15 @@
 package co.coinsmith.kafka.cryptocoin
 
 import akka.actor.{Actor, Props}
-import co.coinsmith.kafka.cryptocoin.streaming.{BitstampStreamingActor, Connect, OKCoinStreamingActor}
+import co.coinsmith.kafka.cryptocoin.streaming.{BitfinexStreamingActor, BitstampStreamingActor, Connect, OKCoinStreamingActor}
 
 
 class StreamingActor extends Actor {
+  val bitfinex = context.actorOf(Props[BitfinexStreamingActor], "bitfinex")
   val bitstamp = context.actorOf(Props[BitstampStreamingActor], "bitstamp")
   val okcoin = context.actorOf(Props[OKCoinStreamingActor], "okcoin")
 
+  bitfinex ! Connect
   bitstamp ! Connect
   okcoin ! Connect
 
---
+++ src/main/scala/co/coinsmith/kafka/cryptocoin/streaming/BitfinexStreamingActor.scala
@@ -0,0 +1,98 @@
+package co.coinsmith.kafka.cryptocoin.streaming
+
+import java.net.URI
+import java.time.Instant
+import javax.websocket.Session
+
+import co.coinsmith.kafka.cryptocoin.KafkaProducer
+import org.json4s.JsonAST._
+import org.json4s.JsonDSL.WithBigDecimal._
+import org.json4s.jackson.JsonMethods._
+
+
+class BitfinexStreamingActor extends ExchangeStreamingActor {
+  val name = "Bitfinex"
+  val uri = new URI("wss://api2.bitfinex.com:3000/ws")
+
+  var subscribed = Map.empty[BigInt, String]
+  def getChannelName(channelId: BigInt) = subscribed(channelId)
+  def topic(channelId: BigInt, updateType: String): String = {
+    (getChannelName(channelId), updateType) match {
+      case ("book", "snapshot") => "streaming.btcusd.orderbook.snapshots"
+      case ("book", "update") => "streaming.btcusd.orderbook"
+      case ("trades", "snapshot") => "streaming.btcusd.trades.snapshots"
+      case ("trades", "tu") => "streaming.btcusd.trades"
+      case ("trades", "te") => "streaming.btcusd.trades.executions"
+      case ("ticker", "ticker") => "streaming.btcusd.ticker"
+    }
+  }
+
+  def subscribe(session: Session) = {
+    val channels = List(
+      ("event" -> "subscribe") ~ ("channel" -> "book") ~ ("pair" -> "BTCUSD")
+        ~ ("prec" -> "R0") ~ ("len" -> "100"),
+      ("event" -> "subscribe") ~ ("channel" -> "trades") ~ ("pair" -> "BTCUSD"),
+      ("event" -> "subscribe") ~ ("channel" -> "ticker") ~ ("pair" -> "BTCUSD")
+    )
+    for (channel <- channels) {
+      val msg = compact(render(channel))
+      session.getBasicRemote.sendText(msg)
+      log.debug("Sent subscription message: {}", msg)
+    }
+  }
+
+  def processOrder(order: JValue) = {
+    ("id" -> order(0)) ~
+      ("price" -> order(1)) ~
+      ("volume" -> order(2))
+  }
+
+  def isEvent(event: JValue): Boolean = event.findField {
+    case ("event", eventName: JString) => true
+    case _ => false
+  } match {
+    case None => false
+    case _ => true
+  }
+
+  def receive = {
+    case Connect => connect
+    case (t, JObject(JField("event", JString("subscribed")) ::
+                     JField("channel", JString(channelName)) ::
+                     JField("chanId", JInt(channelId)) ::
+                     JField("pair", JString("BTCUSD")) ::
+                     xs)) =>
+      log.info("Received subscription event response for channel {} with ID {}", channelName, channelId)
+      subscribed += (channelId -> channelName)
+    case (t, event: JValue) if isEvent(event) =>
+      log.info("Received event message: {}", compact(render(event)))
+    case (t: Instant, JArray(JInt(channelId) :: JString("hb") :: Nil)) =>
+      log.debug("Received heartbeat message for channel ID {}", channelId)
+    case (t: Instant, JArray(JInt(channelId) :: JArray(data) :: Nil)) =>
+      val json = getChannelName(channelId) match {
+        case "book" => data map processOrder
+        case "trades" => data map { t =>
+          val seqOrId = t(0) match {
+            case seq: JString => ("seq" -> seq)
+            case id: JInt => ("id" -> id)
+            case _ => throw new Exception(s"Trade snapshot processing error for $t")
+          }
+          seqOrId ~ ("timestamp" -> t(1)) ~ ("price" -> t(2)) ~ ("volume" -> t(3))
+        }
+        case _ => throw new Exception("Snapshot does not have nested array structure.")
+      }
+      self ! (topic(channelId, "snapshot"), JArray(json))
+    case (t: Instant, JArray(JInt(channelId) :: JString(updateType) :: xs)) =>
+      self ! (topic(channelId, updateType), JArray(xs))
+    case (t: Instant, JArray(JInt(channelId) :: xs)) =>
+      val updateType = xs.length match {
+        case 3 => "update"
+        case 10 => "ticker"
+      }
+      self ! (topic(channelId, updateType), JArray(xs))
+    case (topic: String, json: JValue) =>
+      val msg = compact(render(json))
+      KafkaProducer.send(name.toLowerCase + "." + topic, null, msg)
+    case m => throw new Exception(s"Unhandled message: $m")
+  }
+}
