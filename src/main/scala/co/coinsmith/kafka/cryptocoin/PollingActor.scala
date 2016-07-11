3e6d9475a41e3983a5d8230128daad28a59fc296
Brandon Bradley <bradleytastic@gmail.com>
2016-07-11 15:23:56 -0500

    [Polling] fix actor instantiation

--- src/main/scala/co/coinsmith/kafka/cryptocoin/PollingActor.scala
+++ src/main/scala/co/coinsmith/kafka/cryptocoin/PollingActor.scala
@@ -1,12 +1,14 @@
 package co.coinsmith.kafka.cryptocoin
 
 import akka.actor.{Actor, Props}
-import polling.{BitfinexPollingActor, BitstampPollingActor}
+import polling.{BitfinexPollingActor, BitstampPollingActor, OKCoinPollingActor}
 
 
 class PollingActor extends Actor {
-  val bitstamp = context.actorOf(Props[BitstampPollingActor])
-  val bitfinex = context.actorOf(Props[BitfinexPollingActor])
+  val bitstamp = context.actorOf(Props[BitstampPollingActor], "bitstamp")
+  val bitfinex = context.actorOf(Props[BitfinexPollingActor], "bitfinex")
+  val okcoin = context.actorOf(Props[OKCoinPollingActor], "okcoin")
+
   def receive = {
     case _ =>
   }
