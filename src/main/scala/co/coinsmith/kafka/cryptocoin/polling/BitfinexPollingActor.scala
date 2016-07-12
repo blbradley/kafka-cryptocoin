7a0a1e47537518f8ee16d318b861c45596445bbb
Brandon Bradley <bradleytastic@gmail.com>
2016-07-11 19:14:58 -0500

    [Polling][Bitfinex] migrate to Akka Streams for HTTP response processing

--- src/main/scala/co/coinsmith/kafka/cryptocoin/polling/BitfinexPollingActor.scala
+++ src/main/scala/co/coinsmith/kafka/cryptocoin/polling/BitfinexPollingActor.scala
@@ -1,13 +1,11 @@
 package co.coinsmith.kafka.cryptocoin.polling
 
-import scala.concurrent.Future
 import java.time.Instant
 
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpEntity, HttpRequest, ResponseEntity}
-import akka.stream.scaladsl.{Flow, Sink, Source}
-import akka.util.ByteString
-import co.coinsmith.kafka.cryptocoin.{KafkaProducer, Order, Utils}
+import akka.http.scaladsl.model.ResponseEntity
+import akka.stream.scaladsl.Flow
+import co.coinsmith.kafka.cryptocoin.{Order, Utils}
 import org.json4s.JsonAST._
 import org.json4s.JsonDSL.WithBigDecimal._
 import org.json4s.jackson.JsonMethods._
@@ -15,53 +13,39 @@ import org.json4s.jackson.JsonMethods._
 
 class BitfinexPollingActor extends HTTPPollingActor {
   val key = "BitFinex"
-  import context.dispatcher
   val pool = Http(context.system).cachedHostConnectionPoolHttps[String]("api.bitfinex.com")
 
-  val chunkedFlow = Flow[(Instant, ResponseEntity)].map {
-    case (t, HttpEntity.Chunked(_, chunks)) =>
-      chunks.runFold(ByteString()) { (s, chunk) => s ++ chunk.data }
-        .map { s => (t, s.utf8String) }
-    case (_, e) =>
-      throw new Exception(s"Chunked response failed with entity ${e}")
+  val tickFlow = Flow[(Instant, ResponseEntity)].map { case (t, entity) =>
+    parse(responseEntityToString(entity)) transform {
+      case JString(v) => JDecimal(BigDecimal(v))
+    } transformField {
+      case JField("timestamp", JDecimal(v)) =>
+        val parts = v.toString.split('.').map { _.toLong }
+        val timestamp = Instant.ofEpochSecond(parts(0), parts(1))
+        JField("timestamp", JString(timestamp.toString))
+      case JField("last_price", v) => JField("last", v)
+    } merge render("time_collected" -> t.toString)
   }
 
-  def request(uri: String) =
-    Source.single(HttpRequest(uri = uri) -> "")
-      .via(pool)
-      .via(responseFlow)
-      .via(chunkedFlow)
-
-  def topicSink(topic: String) = Sink.foreach[Future[(Instant, String)]] { f =>
-    f.onSuccess { case (t, s) => self ! (t, topic, s) }
+  val orderbookFlow = Flow[(Instant, ResponseEntity)].map { case (t, entity) =>
+    val json = parse(responseEntityToString(entity)) transform {
+      case JString(v) => JDecimal(BigDecimal(v))
+    } transformField {
+      case JField("amount", v) => JField("volume", v)
+    }
+    val asks = (json \ "asks").extract[List[Order]]
+    val bids = (json \ "bids").extract[List[Order]]
+    Utils.orderBookToJson(None, t, asks, bids)
   }
 
   def receive = {
-    case "tick" => request("/v1/pubticker/btcusd").runWith(topicSink("ticks"))
-    case "orderbook" => request("/v1/book/btcusd").runWith(topicSink("orderbooks"))
-    case (t: Instant, "ticks", data: String) =>
-      val json = parse(data) transform {
-        case JString(v) => JDecimal(BigDecimal(v))
-      } transformField {
-        case JField("timestamp", JDecimal(v)) =>
-          val parts = v.toString.split('.').map { _.toLong }
-          val timestamp = Instant.ofEpochSecond(parts(0), parts(1))
-          JField("timestamp", JString(timestamp.toString))
-        case JField("last_price", v) => JField("last", v)
-      } merge render("time_collected" -> t.toString)
-      self ! ("ticks", json)
-    case (t: Instant, "orderbooks", data: String) =>
-      val json = parse(data) transform {
-        case JString(v) => JDecimal(BigDecimal(v))
-      } transformField {
-        case JField("amount", v) => JField("volume", v)
-      }
-      val asks = (json \ "asks").extract[List[Order]]
-      val bids = (json \ "bids").extract[List[Order]]
-      val orderbook = Utils.orderBookToJson(None, t, asks, bids)
-      self ! ("orderbooks", orderbook)
-    case (topic: String, json: JValue) =>
-      val msg = compact(render(json))
-      KafkaProducer.send(topic, key, msg)
+    case "tick" =>
+      request("/v1/pubticker/btcusd")
+        .via(tickFlow)
+        .runWith(kafkaSink("ticks"))
+    case "orderbook" =>
+      request("/v1/book/btcusd")
+        .via(orderbookFlow)
+        .runWith(kafkaSink("orderbooks"))
   }
 }
