dd83df7ca0591005dbd5a9341c6f8d6d2b3f6640
Brandon Bradley <bradleytastic@gmail.com>
2016-07-22 19:29:11 -0500

    [Polling][Bitstamp] migration to Akka Streams for processing

--- src/main/scala/co/coinsmith/kafka/cryptocoin/polling/BitstampPollingActor.scala
+++ src/main/scala/co/coinsmith/kafka/cryptocoin/polling/BitstampPollingActor.scala
@@ -1,14 +1,10 @@
 package co.coinsmith.kafka.cryptocoin.polling
 
-import scala.concurrent.Future
-import scala.util.{Failure, Success, Try}
 import java.time.Instant
 
-import akka.Done
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpEntity, HttpRequest, HttpResponse}
-import akka.stream.scaladsl.{Sink, Source}
-import akka.util.ByteString
+import akka.http.scaladsl.model.ResponseEntity
+import akka.stream.scaladsl.Flow
 import co.coinsmith.kafka.cryptocoin.producer.ProducerBehavior
 import co.coinsmith.kafka.cryptocoin.{Order, Utils}
 import org.json4s.JsonAST._
@@ -18,50 +14,37 @@ import org.json4s.jackson.JsonMethods._
 
 class BitstampPollingActor extends HTTPPollingActor with ProducerBehavior {
   val topicPrefix = "bitstamp.polling.btcusd."
-  import context.dispatcher
   val pool = Http(context.system).cachedHostConnectionPoolHttps[String]("www.bitstamp.net")
 
-  def sink: Sink[(Try[HttpResponse], String), Future[Done]] = {
-    Sink.foreach {
-      case (Success(HttpResponse(statusCode, _, entity, _)), _) =>
-        val timeCollected = Instant.now
-        log.debug("Request returned status code {} with entity {}",  statusCode, entity)
-        self ! (timeCollected, entity)
-      case (Failure(response), _) =>
-        log.debug("Request failed with response {}", response)
+  val tickFlow = Flow[(Instant, ResponseEntity)].map { case (t, entity) =>
+    val json = parse(responseEntityToString(entity)).transformField {
+      case JField("timestamp", JString(t)) => JField("timestamp", JString(Instant.ofEpochSecond(t.toLong).toString))
+      case JField(key, JString(value)) => JField(key, JDecimal(BigDecimal(value)))
+    } merge render("time_collected" -> t.toString)
+    ("ticks", json)
+  }
+
+  val orderbookFlow = Flow[(Instant, ResponseEntity)].map { case (t, entity) =>
+    val json = parse(responseEntityToString(entity)).transformField {
+      case JField("timestamp", JString(t)) => JField("timestamp", JString(Instant.ofEpochSecond(t.toLong).toString))
     }
+    val timestamp = (json \ "timestamp").extract[String]
+    val asks = (json \ "asks").extract[List[List[String]]]
+      .map { o => new Order(BigDecimal(o(0)), BigDecimal(o(1))) }
+    val bids = (json \ "bids").extract[List[List[String]]]
+      .map { o => new Order(BigDecimal(o(0)), BigDecimal(o(1))) }
+    val orderbook = Utils.orderBookToJson(Some(timestamp), t, asks, bids)
+    ("orderbook", orderbook)
   }
 
   def receive = producerBehavior orElse {
     case "tick" =>
-      Source.single(HttpRequest(uri = "/api/ticker/") -> "")
-        .via(pool)
-        .runWith(sink)
+      request("/api/ticker/")
+        .via(tickFlow)
+        .runWith(selfSink)
     case "orderbook" =>
-      Source.single(HttpRequest(uri = "/api/order_book/") -> "")
-        .via(pool)
-        .runWith(sink)
-    case (t: Instant, HttpEntity.Strict(_, data)) =>
-      val json = parse(data.utf8String).transformField {
-        case JField("timestamp", JString(t)) => JField("timestamp", JString(Instant.ofEpochSecond(t.toLong).toString))
-        case JField(key, JString(value)) => JField(key, JDecimal(BigDecimal(value)))
-      } merge render("time_collected" -> t.toString)
-      self ! ("ticks", json)
-
-    case (t: Instant, HttpEntity.Default(_, _, data)) =>
-      data.runWith(Sink.fold[ByteString, ByteString](ByteString())(_ ++ _))
-        .onSuccess {
-          case s =>
-            val json = parse(s.utf8String).transformField {
-              case JField("timestamp", JString(t)) => JField("timestamp", JString(Instant.ofEpochSecond(t.toLong).toString))
-            }
-            val timestamp = (json \ "timestamp").extract[String]
-            val asks = (json \ "asks").extract[List[List[String]]]
-              .map { o => new Order(BigDecimal(o(0)), BigDecimal(o(1))) }
-            val bids = (json \ "bids").extract[List[List[String]]]
-              .map { o => new Order(BigDecimal(o(0)), BigDecimal(o(1))) }
-            val orderbook = Utils.orderBookToJson(Some(timestamp), t, asks, bids)
-            self ! ("orderbook", orderbook)
-        }
+      request("/api/order_book/")
+        .via(orderbookFlow)
+        .runWith(selfSink)
   }
 }
--- src/test/scala/polling/BitstampPollingActorSpec.scala
+++ src/test/scala/polling/BitstampPollingActorSpec.scala
@@ -4,14 +4,19 @@ import java.time.Instant
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Source
 import akka.testkit.TestActorRef
 import akka.util.ByteString
 import co.coinsmith.kafka.cryptocoin.polling.BitstampPollingActor
+import org.json4s.JsonDSL.WithBigDecimal._
+import org.json4s.jackson.JsonMethods._
 
 
 class BitstampPollingActorSpec
   extends HTTPPollingActorSpec(ActorSystem("BitstampPollingActorSpecSystem")) {
+  implicit val materializer = ActorMaterializer()
+
   val actorRef = TestActorRef[BitstampPollingActor]
   val actor = actorRef.underlyingActor
   actor.tick.cancel
@@ -19,27 +24,70 @@ class BitstampPollingActorSpec
 
   "BitstampPollingActor" should "process a ticker message" in {
     val timeCollected = Instant.ofEpochSecond(10L)
+    val json = ("high" ->  "424.37") ~
+      ("last" -> "415.24") ~
+      ("timestamp" -> "1459297128") ~
+      ("bid" -> "414.34") ~
+      ("vwap" -> "415.41") ~
+      ("volume" -> "5961.02582305") ~
+      ("low" -> "407.22") ~
+      ("ask" -> "415.24") ~
+      ("open" -> 415.43)
     val contentType = ContentTypes.`application/json`
-    val data = ByteString(fixture("bitstamp-ticker-response.json"))
+    val data = ByteString(compact(render(json)))
     val entity = HttpEntity.Strict(contentType, data)
+
+    val expected = ("high" -> 424.37) ~
+      ("last" -> 415.24) ~
+      ("timestamp" -> "2016-03-30T00:18:48Z") ~
+      ("bid" -> 414.34) ~
+      ("vwap" -> 415.41) ~
+      ("volume" -> 5961.02582305) ~
+      ("low" -> 407.22) ~
+      ("ask" -> 415.24) ~
+      ("open" -> 415.43) ~
+      ("time_collected" -> "1970-01-01T00:00:10Z")
     withRunningKafka {
-      actorRef ! (timeCollected, entity)
-      val expected = fixture("bitstamp-ticker-kafka.json")
-      val result = consumeFirstStringMessageFrom("bitstamp.polling.btcusd.ticks")
+      Source.single((timeCollected, entity))
+        .via(actor.tickFlow)
+        .runWith(actor.selfSink)
+      val msg = consumeFirstStringMessageFrom("bitstamp.polling.btcusd.ticks")
+      val result = parse(msg, true)
       assert(result == expected)
     }
   }
 
   it should "process an orderbook message" in {
     val timeCollected = Instant.ofEpochSecond(10L)
+    val json = ("timestamp" -> "1461605735") ~
+      ("bids" -> List(
+        List("462.49", "0.03010000"),
+        List("462.48", "4.03000000"),
+        List("462.47", "16.49799877")
+      )) ~ ("asks" -> List(
+        List("462.50", "9.12686646"),
+        List("462.51", "0.05981955"),
+        List("462.88", "1.00000000")
+      ))
     val contentType = ContentTypes.`application/json`
-    val data = ByteString(fixture("bitstamp-orderbook-response.json"))
+    val data = ByteString(compact(render(json)))
     val source = Source.single(data)
     val entity = HttpEntity.Default(contentType, data.length, source)
+
+    val expected = ("timestamp" -> "2016-04-25T17:35:35Z") ~
+      ("time_collected" -> "1970-01-01T00:00:10Z") ~
+      ("ask_prices" -> List(BigDecimal("462.50"), BigDecimal("462.51"), BigDecimal("462.88"))) ~
+      ("ask_volumes" -> List(BigDecimal("9.12686646"), BigDecimal("0.05981955"), BigDecimal("1.00000000"))) ~
+      ("ask_timestamps" -> List[BigDecimal]()) ~
+      ("bid_prices" -> List(BigDecimal("462.49"), BigDecimal("462.48"), BigDecimal("462.47"))) ~
+      ("bid_volumes" -> List(BigDecimal("0.03010000"), BigDecimal("4.03000000"), BigDecimal("16.49799877"))) ~
+      ("bid_timestamps" -> List[BigDecimal]())
     withRunningKafka {
-      actorRef ! (timeCollected, entity)
-      val expected = fixture("bitstamp-orderbook-kafka.json")
-      val result = consumeFirstStringMessageFrom("bitstamp.polling.btcusd.orderbook")
+      Source.single((timeCollected, entity))
+        .via(actor.orderbookFlow)
+        .runWith(actor.selfSink)
+      val msg = consumeFirstStringMessageFrom("bitstamp.polling.btcusd.orderbook")
+      val result = parse(msg, true)
       assert(result == expected)
     }
   }
