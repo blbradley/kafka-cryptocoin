6dab8c93cf0469015ee50e580219ca7a24eb4033
Brandon Bradley <bradleytastic@gmail.com>
2016-08-10 17:22:54 -0500

    WebsocketActor and protocol usage for Bitfinex

--- src/main/scala/co/coinsmith/kafka/cryptocoin/streaming/BitfinexStreamingActor.scala
+++ src/main/scala/co/coinsmith/kafka/cryptocoin/streaming/BitfinexStreamingActor.scala
@@ -2,18 +2,15 @@ package co.coinsmith.kafka.cryptocoin.streaming
 
 import java.net.URI
 import java.time.Instant
-import javax.websocket.Session
 
+import akka.actor.{Actor, ActorLogging, Props}
 import co.coinsmith.kafka.cryptocoin.producer.ProducerBehavior
 import org.json4s.JsonAST._
 import org.json4s.JsonDSL.WithBigDecimal._
 import org.json4s.jackson.JsonMethods._
 
 
-class BitfinexStreamingActor extends ExchangeStreamingActor with ProducerBehavior {
-  val topicPrefix = "bitfinex.streaming.btcusd."
-  val uri = new URI("wss://api2.bitfinex.com:3000/ws")
-
+class BitfinexWebsocketProtocol extends Actor with ActorLogging {
   var subscribed = Map.empty[BigInt, String]
   def getChannelName(channelId: BigInt) = subscribed(channelId)
   def topic(channelId: BigInt, updateType: String): String = {
@@ -24,20 +21,6 @@ class BitfinexStreamingActor extends ExchangeStreamingActor with ProducerBehavio
       case ("trades", "tu")       => "trades"
       case ("trades", "te")       => "trades.executions"
       case ("ticker", "ticker")   => "ticker"
-    }
-  }
-
-  def subscribe(session: Session) = {
-    val channels = List(
-      ("event" -> "subscribe") ~ ("channel" -> "book") ~ ("pair" -> "BTCUSD")
-        ~ ("prec" -> "R0") ~ ("len" -> "100"),
-      ("event" -> "subscribe") ~ ("channel" -> "trades") ~ ("pair" -> "BTCUSD"),
-      ("event" -> "subscribe") ~ ("channel" -> "ticker") ~ ("pair" -> "BTCUSD")
-    )
-    for (channel <- channels) {
-      val msg = compact(render(channel))
-      session.getBasicRemote.sendText(msg)
-      log.debug("Sent subscription message: {}", msg)
     }
   }
 
@@ -55,8 +38,7 @@ class BitfinexStreamingActor extends ExchangeStreamingActor with ProducerBehavio
     case _ => true
   }
 
-  def receive = producerBehavior orElse {
-    case Connect => connect
+  def receive = {
     case (t, JObject(JField("event", JString("subscribed")) ::
                      JField("channel", JString(channelName)) ::
                      JField("chanId", JInt(channelId)) ::
@@ -81,15 +63,44 @@ class BitfinexStreamingActor extends ExchangeStreamingActor with ProducerBehavio
         }
         case _ => throw new Exception("Snapshot does not have nested array structure.")
       }
-      self ! (topic(channelId, "snapshot"), JArray(json))
+      sender ! (topic(channelId, "snapshot"), JArray(json))
     case (t: Instant, JArray(JInt(channelId) :: JString(updateType) :: xs)) =>
-      self ! (topic(channelId, updateType), JArray(xs))
+      sender ! (topic(channelId, updateType), JArray(xs))
     case (t: Instant, JArray(JInt(channelId) :: xs)) =>
       val updateType = xs.length match {
         case 3 => "update"
         case 10 => "ticker"
       }
-      self ! (topic(channelId, updateType), JArray(xs))
+      sender ! (topic(channelId, updateType), JArray(xs))
     case m => throw new Exception(s"Unhandled message: $m")
+  }
+}
+
+class BitfinexStreamingActor extends Actor with ActorLogging with ProducerBehavior {
+  val topicPrefix = "bitfinex.streaming.btcusd."
+  val uri = new URI("wss://api2.bitfinex.com:3000/ws")
+
+  val websocket = context.actorOf(WebsocketActor.props(uri))
+  val protocol = context.actorOf(Props[BitfinexWebsocketProtocol])
+
+  val channels = List(
+    ("event" -> "subscribe") ~ ("channel" -> "book") ~ ("pair" -> "BTCUSD")
+      ~ ("prec" -> "R0") ~ ("len" -> "100"),
+    ("event" -> "subscribe") ~ ("channel" -> "trades") ~ ("pair" -> "BTCUSD"),
+    ("event" -> "subscribe") ~ ("channel" -> "ticker") ~ ("pair" -> "BTCUSD")
+  )
+
+  override def preStart = {
+    websocket ! self
+    websocket ! Connect
+    for (channel <- channels) {
+      val msg = compact(render(channel))
+      websocket ! msg
+      log.debug("Sent subscription message: {}", msg)
+    }
+  }
+
+  def receive = producerBehavior orElse {
+    case (t: Instant, json: JValue) => protocol ! (t, json)
   }
 }
