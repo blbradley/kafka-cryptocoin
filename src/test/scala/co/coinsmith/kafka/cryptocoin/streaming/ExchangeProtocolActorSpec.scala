a5ed331b54b3a8e13829014630d71681d16762db
Brandon Bradley <bradleytastic@gmail.com>
2016-12-15 12:47:57 -0600

    Revert "[Streaming][Bitfinex] throw exception on info event with code 20051" (#104)

--- src/test/scala/co/coinsmith/kafka/cryptocoin/streaming/ExchangeProtocolActorSpec.scala
+++ src/test/scala/co/coinsmith/kafka/cryptocoin/streaming/ExchangeProtocolActorSpec.scala
@@ -2,11 +2,11 @@ package co.coinsmith.kafka.cryptocoin.streaming
 
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit}
-import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike}
 
 
 abstract class ExchangeProtocolActorSpec(_system: ActorSystem) extends TestKit(_system)
-  with FlatSpecLike with BeforeAndAfterAll with Matchers with ImplicitSender {
+  with FlatSpecLike with BeforeAndAfterAll with ImplicitSender {
   override def afterAll {
     TestKit.shutdownActorSystem(system)
   }
