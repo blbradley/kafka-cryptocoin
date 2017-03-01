be59c98e5637ee9714d69f09791476522bfc731e
Brandon Bradley <bradleytastic@gmail.com>
2017-02-28 18:45:43 -0600

    check for Schema Registry reachability at startup (#114)

--- src/main/scala/co/coinsmith/kafka/cryptocoin/KafkaCryptocoin.scala
+++ src/main/scala/co/coinsmith/kafka/cryptocoin/KafkaCryptocoin.scala
@@ -1,11 +1,40 @@
 package co.coinsmith.kafka.cryptocoin
 
+import java.io.IOException
+import java.net.{MalformedURLException, URL}
+
 import akka.actor.{ActorSystem, Props}
+import akka.event.Logging
+import com.typesafe.config.ConfigFactory
 
 
 object KafkaCryptocoin {
   implicit val system = ActorSystem("KafkaCryptocoinSystem")
+  val log = Logging(system.eventStream, this.getClass.getName)
+
+  val conf = ConfigFactory.load
+  val schemaRegistryUrl = conf.getString("kafka.cryptocoin.schema-registry-url")
+
+  def isSchemaRegistryAvailable: Boolean = {
+    val connection = new URL(schemaRegistryUrl).openConnection
+    try {
+      connection.connect
+      return true
+    } catch  {
+      case e: MalformedURLException =>
+        throw new RuntimeException("Schema Registry URL is malformed.")
+      case e: IOException =>
+        log.error(e, "Schema registry at {} is unreachable.", schemaRegistryUrl)
+        return false
+    }
+  }
+
   def main(args: Array[String]) {
+    while(isSchemaRegistryAvailable == false) {
+      log.info("Retrying connection to schema registry in three seconds.")
+      Thread.sleep(3000)
+    }
+
     val streamingActor = system.actorOf(Props[StreamingActor], "streaming")
     val pollingActor = system.actorOf(Props[PollingActor], "polling")
   }
