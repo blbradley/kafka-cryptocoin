2cfbf25dcdff4a9edbe761002dc3b32c7e5b2022
Brandon Bradley <bradleytastic@gmail.com>
2017-05-16 11:20:25 -0500

    [EXPERIMENTAL] write to file when broker is down

--- src/main/scala/co/coinsmith/kafka/cryptocoin/producer/KafkaCryptocoinProducer.scala
+++ src/main/scala/co/coinsmith/kafka/cryptocoin/producer/KafkaCryptocoinProducer.scala
@@ -1,7 +1,8 @@
 package co.coinsmith.kafka.cryptocoin.producer
 
 import scala.util.{Failure, Success, Try}
-import java.io.FileInputStream
+import java.io.{ByteArrayOutputStream, FileInputStream, ObjectOutputStream}
+import java.nio.file.Paths
 import java.util.{Properties, UUID}
 
 import akka.Done
@@ -9,15 +10,21 @@ import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.kafka.ProducerSettings
 import akka.kafka.scaladsl.Producer
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{FileIO, Flow, Source}
+import akka.stream.{ActorMaterializer, OverflowStrategy}
+import akka.util.ByteString
 import co.coinsmith.kafka.cryptocoin.KafkaCryptocoin
 import com.typesafe.config.ConfigFactory
 import io.confluent.kafka.serializers.KafkaAvroSerializer
+import org.apache.kafka.common.serialization
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
+import org.apache.kafka.common.errors.TimeoutException
+import org.apache.kafka.common.serialization.Serializer
 
 object KafkaCryptocoinProducer {
-  val logger = Logging(KafkaCryptocoin.system.eventStream, this.getClass.getName)
+  import KafkaCryptocoin.system
+  implicit val materializer = ActorMaterializer()
+  val logger = Logging(system.eventStream, this.getClass.getName)
 
   val uuid = UUID.randomUUID
   logger.info("Producer UUID: {}", uuid)
@@ -42,21 +49,45 @@ object KafkaCryptocoinProducer {
   props.put("schema.registry.url", schemaRegistryUrl)
   val producer = new KafkaProducer[Object, Object](props)
 
+  val keySerializerField = producer.getClass.getDeclaredField("keySerializer")
+  keySerializerField.setAccessible(true)
+  val keySerializer = keySerializerField.get(producer).asInstanceOf[Serializer[Object]]
+  println(keySerializer)
+
+  val valueSerializerField = producer.getClass.getDeclaredField("valueSerializer")
+  valueSerializerField.setAccessible(true)
+  val valueSerializer = valueSerializerField.get(producer).asInstanceOf[Serializer[Object]]
+  println(valueSerializer)
+
+  val queueSize = 1000
+  val queueSource = Source.queue[ProducerRecord[Object, Object]](queueSize, OverflowStrategy.backpressure)
+  val recordByteFlow = Flow[ProducerRecord[Object, Object]].map { record =>
+    val byteStream = new ByteArrayOutputStream()
+    val objectStream = new ObjectOutputStream(byteStream)
+    val serializedKey = keySerializer.serialize(record.topic, record.key)
+    val serializedValue =valueSerializer.serialize(record.topic, record.value)
+    objectStream.writeObject((record.topic, serializedKey, serializedValue))
+    objectStream.close
+    ByteString(byteStream.toByteArray)
+  }
+  val fileSink = FileIO.toPath(Paths.get("/tmp/kafka-cryptocoin-log"))
+  val queue = queueSource.via(recordByteFlow).to(fileSink).run
+
   def getProducerSink(implicit system: ActorSystem) = {
     val settings = ProducerSettings(system, new KafkaAvroSerializer, new KafkaAvroSerializer)
     Producer.plainSink(settings, producer)
   }
 
-  def producerComplete(td: Try[Done]) = td match {
+  def producerComplete(record: ProducerRecord[Object, Object])(td: Try[Done]) = td match {
     case Success(d) =>
-    case Failure(ex) => throw ex
+    case Failure(ex) => queue.offer(record)
   }
 
   def send(topic: String, key: Object, value: Object)(implicit system: ActorSystem, materializer: ActorMaterializer) = {
     import system.dispatcher
     val data = new ProducerRecord[Object, Object](topic, key, value)
     val closed = Source.single(data).runWith(getProducerSink)
-    closed.onComplete(producerComplete)
+    closed.onComplete(producerComplete(data))
   }
 
   def send(topic: String, value: Object)(implicit system: ActorSystem, materializer: ActorMaterializer): Unit =
